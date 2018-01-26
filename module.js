d86824eb40604a4e4a25c1cb717b280508e6a457
Unknown <paul86677889@gmail.com>
2018-01-26 18:00:33 +0800

    change

--- module.js
+++ module.js
@@ -192,12 +192,12 @@ var ModuleName = 'frzTable';
 var ModuleDefaults = {
 	count: {
 		// M版時每次點擊往前往後移動幾格儲存格
-		slide: 2, // [number] 
+		slide: 3, // [number] 
 		// M版時一個畫面show幾格儲存格
 		show: 5 // [number] 
 	},
 	// 設定花多久時間移動完成
-	speed: .2, // [number] 
+	speed: .3, // [number] 
 	// 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
 	whenClick: function whenClick($element) {
 		// console.log($element)
@@ -239,12 +239,9 @@ var Module = function () {
 					Module.prototype.goLeftScroll();
 				}
 			});
-
 			this.changeShow();
 			this.selectBox();
 			// this.addColNum();
-			// this.srcollLeft();
-			// this.srcollRight();
 			return this;
 		}
 	}, {
@@ -263,9 +260,9 @@ var Module = function () {
 			$(".content_box2").on('click', function () {
 				$(".content_box2").removeClass('select').removeClass('hight_light');
 				$(this).addClass('select').siblings().addClass('hight_light');
-				var i = $('.select').index() + 1;
-				console.log(i);
-				$(".content_box2:nth-child(" + i + ")").addClass("hight_light");
+				var selectIndex = $('.select').index() + 1; //:nth-child()的索引值從1開始
+				console.log(selectIndex);
+				$(".content_box2:nth-child(" + selectIndex + ")").addClass("hight_light");
 				$(this).removeClass('hight_light');
 			});
 			return this;
@@ -273,7 +270,7 @@ var Module = function () {
 	}, {
 		key: 'changeShow',
 		value: function changeShow() {
-			var borderSpace = ModuleDefaults.count.show * 2;
+			var borderSpace = ModuleDefaults.count.show * 1;
 			var BoxShow = ($(".col-xs-21").width() - borderSpace) / ModuleDefaults.count.show;
 			console.log(BoxShow);
 			$(".content_box2").width(BoxShow);
@@ -284,7 +281,7 @@ var Module = function () {
 		key: 'goRightScroll',
 		value: function goRightScroll() {
 			var srcollSpeed = ModuleDefaults.speed * 1000;
-			var srcollWidth = ($('.content_box2').width() + 2) * ModuleDefaults.count.slide;
+			var srcollWidth = ($('.content_box2').width() + 1) * ModuleDefaults.count.slide;
 			$(".content_box2").animate({
 				left: "+=" + srcollWidth + ""
 			}, srcollSpeed);
@@ -294,7 +291,7 @@ var Module = function () {
 		key: 'goLeftScroll',
 		value: function goLeftScroll() {
 			var srcollSpeed = ModuleDefaults.speed * 1000;
-			var srcollWidth = ($('.content_box2').width() + 2) * ModuleDefaults.count.slide;
+			var srcollWidth = ($('.content_box2').width() + 1) * ModuleDefaults.count.slide;
 			$(".content_box2").animate({
 				left: "-=" + srcollWidth + ""
 			}, srcollSpeed);
--- script/module.js
+++ script/module.js
@@ -2,12 +2,12 @@ const ModuleName = 'frzTable';
 const ModuleDefaults =  {
 	count: {
         // M版時每次點擊往前往後移動幾格儲存格
-        slide: 2, // [number] 
+        slide: 3, // [number] 
         // M版時一個畫面show幾格儲存格
         show: 5// [number] 
     },
     // 設定花多久時間移動完成
-    speed: .2, // [number] 
+    speed: .3, // [number] 
     // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
     whenClick: function($element) {
         // console.log($element)
@@ -44,12 +44,9 @@ class Module {
 		        Module.prototype.goLeftScroll();
 		      }
 		});	
-		
 		this.changeShow();
 		this.selectBox();
 		// this.addColNum();
-		// this.srcollLeft();
-		// this.srcollRight();
 		return this;
 	}
 	methods () {
@@ -62,15 +59,15 @@ class Module {
 		$(".content_box2").on('click',function(){
 			$(".content_box2").removeClass('select').removeClass('hight_light');
 			$(this).addClass('select').siblings().addClass('hight_light');
-			var i = $('.select').index()+1;
-			console.log(i);
-			$(".content_box2:nth-child(" + i +")").addClass( "hight_light" );
+			var selectIndex = $('.select').index()+1;//:nth-child()的索引值從1開始
+			console.log(selectIndex);
+			$(".content_box2:nth-child(" + selectIndex +")").addClass( "hight_light" );
 			$(this).removeClass('hight_light');
 		});
 		return this;
 	}
 	changeShow(){
-		var borderSpace= ModuleDefaults.count.show * 2;
+		var borderSpace= ModuleDefaults.count.show * 1;
 		var BoxShow=($(".col-xs-21").width()-borderSpace) / ModuleDefaults.count.show;
 		console.log(BoxShow);
 		$(".content_box2").width(BoxShow);
@@ -79,7 +76,7 @@ class Module {
 	}
 	goRightScroll(){
 		var srcollSpeed=ModuleDefaults.speed*1000;
-		var srcollWidth= ($('.content_box2').width()+2)*ModuleDefaults.count.slide;
+		var srcollWidth= ($('.content_box2').width()+1)*ModuleDefaults.count.slide;
 		$( ".content_box2" ).animate({
 					left: "+="+srcollWidth+"",
 				},srcollSpeed);
@@ -87,7 +84,7 @@ class Module {
 	}
 	goLeftScroll(){
 		var srcollSpeed=ModuleDefaults.speed*1000;
-		var srcollWidth= ($('.content_box2').width()+2)*ModuleDefaults.count.slide;
+		var srcollWidth= ($('.content_box2').width()+1)*ModuleDefaults.count.slide;
 		$( ".content_box2" ).animate({
 					left: "-="+srcollWidth+"",
 				},srcollSpeed);
